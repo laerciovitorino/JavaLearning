17f5b01515d4a589287f44e02e54f47a64e829af
laerciovitorino <laerciovitorino@gmail.com>
2015-03-20 14:22:26 -0300

    - Point class created.

---
+++ JavaLearning/src/chapter1/Point.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+public class Point {
+	public double x, y;
+	
+	public void clear(){
+		this.x = 0.0;
+		this.y = 0.0;
+	}
+	
+	public double distance(Point that){
+		double xdiff = x - that.x;
+		double ydiff = y - that.y;
+		return Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+	}
+	
+	public void move(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void move(Point that){
+		that.x = this.x;
+		that.y = this.y;
+	}
+}
